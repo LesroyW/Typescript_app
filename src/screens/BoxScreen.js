6f606e747612a377510f700181670d98a9e404e3
Lesroy Wedderburn <wlesroy98@outlook.com>
2023-06-21 23:17:57 +0100

    Completion of Box section of tutorial

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,9 +3,9 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Child #1</Text>
-        <Text style={styles.textStyle}>Child #2</Text>
-        <Text style={styles.textStyle}>Child #3</Text>
+        <View style={styles.textStyle1}><Text>Child #1</Text></View>
+        <View style={styles.textStyle2}><Text>Child #2</Text></View>
+        <View style={styles.textStyle3}><Text>Child #3</Text></View>
     </View>
 }
 
@@ -13,12 +13,29 @@ const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
         borderColor: 'black',
-        height: 200,
-        justifyContent: 'space-around'
+        height: 500
     },
-    textStyle: {
+    textStyle1: {
         borderWidth: 3,
         borderColor: 'red',
+        height: 110,
+        width: 110,
+        alignSelf: 'flex-start'
+    },
+    textStyle2: {
+        borderWidth: 3,
+        borderColor: 'red',
+        height: 110,
+        width: 110,
+        alignSelf: 'center'
+    },
+    textStyle3: {
+        borderWidth: 3,
+        borderColor: 'red',
+        height: 110,
+        width: 110,
+        position: 'absolute',
+        alignSelf: 'flex-end'
     }
 });
 
